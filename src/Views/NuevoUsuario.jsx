b4d0fb4217e833b387c5eb196f24a2190339e855
Smogg21 <spazzaglia98@gmail.com>
2024-10-31 12:15:11 -0500

    Nuevo Usuario ahora funciona

--- src/Views/NuevoUsuario.jsx
+++ src/Views/NuevoUsuario.jsx
@@ -1,15 +1,237 @@
-import { useRoles } from "../Hooks/useRoles"
-
+import { useNavigate } from "react-router-dom";
+import { useRoles } from "../Hooks/useRoles";
+import Select from "react-select";
+import { useState } from "react";
+import axios from "axios";
 
 export const NuevoUsuario = () => {
+  const { roles, loading } = useRoles();
+  const navigate = useNavigate();
 
-  const { roles } = useRoles();
+  const opcionesRoles = roles.map((rol) => ({
+    value: rol.IdRol,
+    label: rol.Nombre,
+  }));
 
+  const [selectedRol, setSelectedRol] = useState(null);
+  const [formValues, setFormValues] = useState({
+    usuario: "",
+    nombre: "",
+    apellidoPaterno: "",
+    contraseña: "",
+  });
 
+  const [mensaje, setMensaje] = useState(null);
+  const handleSelectChange = (selected) => {
+    setSelectedRol(selected);
+  };
 
-  return(
-    <div>
-      <h1>Agregar Usuario</h1>
+  const handleInputChange = (e) => {
+    const { name, type, checked, value } = e.target;
+
+    if (type === "checkbox") {
+      setFormValues({ ...formValues, [name]: checked });
+    } else {
+      setFormValues({ ...formValues, [name]: value });
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Resetear mensaje previo
+    setMensaje(null);
+
+    // Validar campos requeridos
+    if (!formValues.usuario.trim()) {
+      setMensaje({
+        tipo: "error",
+        texto: "El usuario es requerido.",
+      });
+      return;
+    }
+    if (!formValues.nombre.trim()) {
+      setMensaje({
+        tipo: "error",
+        texto: "El nombre es requerido.",
+      });
+      return;
+    }
+    if (!formValues.apellidoPaterno.trim()) {
+      setMensaje({
+        tipo: "error",
+        texto: "El apellido paterno es requerido.",
+      });
+      return;
+    }
+
+    if (!selectedRol) {
+      setMensaje({
+        tipo: "error",
+        texto: "Por favor, selecciona un rol.",
+      });
+      return;
+    }
+
+    const response = await axios.post("http://localhost:5000/api/nuevoUsuario", {
+      ...formValues,
+      IdRol: selectedRol.value,
+    });
+
+    if (response.status === 201) {
+      navigate("/vistaGestionUsuarios", {
+        state: { mensaje: { tipo: "exito", texto: "Usuario creado exitosamente." } },
+      });
+    } else {
+      setMensaje({
+        tipo: "error",
+        texto: "Error al crear el usuario.",
+      });
+    }
+  };
+
+  const selectStyles = {
+    control: (provided) => ({
+      ...provided,
+      backgroundColor: "#4a4a4a",
+      color: "white",
+    }),
+    menu: (provided) => ({
+      ...provided,
+      backgroundColor: "#221f22",
+      color: "white",
+    }),
+    option: (provided, state) => ({
+      ...provided,
+      backgroundColor: state.isSelected
+        ? "#221f22"
+        : state.isFocused
+        ? "black"
+        : "gray",
+      color: "white",
+    }),
+    singleValue: (provided) => ({
+      ...provided,
+      color: "white",
+    }),
+    input: (provided) => ({
+      ...provided,
+      color: "white",
+    }),
+  };
+
+  return (
+    <div
+      style={{
+        maxWidth: "500px",
+        margin: "0 auto",
+        padding: "20px",
+        backgroundColor: "#333",
+        color: "white",
+        borderRadius: "8px",
+      }}
+    >
+      <h1>Nuevo Usuario</h1>
+      {mensaje && (
+        <div
+          style={{
+            padding: "10px",
+            marginBottom: "15px",
+            color: mensaje.tipo === "exito" ? "green" : "red",
+            border: `1px solid ${mensaje.tipo === "exito" ? "green" : "red"}`,
+            borderRadius: "4px",
+            backgroundColor: mensaje.tipo === "exito" ? "#d4edda" : "#f8d7da",
+          }}
+        >
+          {mensaje.texto}
+        </div>
+      )}
+
+      <form
+        onSubmit={handleSubmit}
+        style={{ display: "flex", flexDirection: "column" }}
+      >
+        <label style={{ marginBottom: "5px" }}>Usuario</label>
+        <input
+          type="text"
+          name="usuario"
+          value={formValues.usuario}
+          onChange={handleInputChange}
+          required
+          style={{
+            padding: "8px",
+            borderRadius: "4px",
+            border: "1px solid #ccc",
+          }}
+        />
+        <label style={{ marginTop: "10px", marginBottom: "5px" }}>Nombre</label>
+        <input
+          type="text"
+          name="nombre"
+          value={formValues.nombre}
+          onChange={handleInputChange}
+          required
+          style={{
+            padding: "8px",
+            borderRadius: "4px",
+            border: "1px solid #ccc",
+          }}
+        />
+        <label style={{ marginTop: "10px", marginBottom: "5px" }}>
+          Apellido Paterno
+        </label>
+        <input
+          type="text"
+          name="apellidoPaterno"
+          value={formValues.apellidoPaterno}
+          onChange={handleInputChange}
+          required
+          style={{
+            padding: "8px",
+            borderRadius: "4px",
+            border: "1px solid #ccc",
+          }}
+        />
+
+        <label style={{ marginTop: "10px", marginBottom: "5px" }}>
+          Contraseña
+        </label>
+        <input
+          type="password"
+          name="contraseña"
+          value={formValues.contraseña}
+          onChange={handleInputChange}
+          required
+          style={{
+            padding: "8px",
+            borderRadius: "4px",
+            border: "1px solid #ccc",
+          }}
+        />
+        <label style={{ marginTop: "10px" }}>Rol</label>
+        {loading ? (
+          <p>Cargando Roles...</p>
+        ) : (
+          <Select
+            options={opcionesRoles}
+            value={selectedRol}
+            onChange={handleSelectChange}
+            styles={selectStyles}
+            placeholder="Selecciona un rol"
+          />
+        )}
+
+        <button type="submit" className="button3" style={{ marginTop: "30px"}}>
+          Crear Usuario
+        </button>
+
+        <button
+          onClick={() => navigate("/vistaGestionUsuarios")}
+          style={{ marginTop: "20px" }}
+        >
+          Regresar
+        </button>
+      </form>
     </div>
-  )
-}
+  );
+};
--- src/Views/VistaGestionUsuarios.jsx
+++ src/Views/VistaGestionUsuarios.jsx
@@ -1,11 +1,17 @@
 import { useNavigate } from "react-router-dom";
+import { useLocation } from 'react-router-dom';
 
 export const VistaGestionUsuarios = () => {
+  const location = useLocation();
+  const mensaje = location.state?.mensaje;
   const navigate = useNavigate();
 
   return (
     <div>
+      
       <h1>Gestion Usuarios</h1>
+
+      
       <div
         style={{
           display: "flex",
@@ -17,7 +23,7 @@ export const VistaGestionUsuarios = () => {
         <button onClick={() => navigate("/nuevoUsuario")} className="button1">
           Agregar Usuario
         </button>
-        <button onClick={() => navigate("/vistaGerente")} className="button1">
+        <button onClick={() => navigate("/editarUsuario")} className="button1">
           Editar Usuario
         </button>
         <button
@@ -32,6 +38,22 @@ export const VistaGestionUsuarios = () => {
         >
           Regresar
         </button>
+
+        {mensaje && (
+        <div
+          style={{
+            padding: "10px",
+            marginTop: "15px",
+            marginBottom: "15px",
+            color: mensaje.tipo === "exito" ? "green" : "red",
+            border: `1px solid ${mensaje.tipo === "exito" ? "green" : "red"}`,
+            borderRadius: "4px",
+            backgroundColor: mensaje.tipo === "exito" ? "#d4edda" : "#f8d7da",
+          }}
+        >
+          {mensaje.texto}
+        </div>
+      )}
     </div>
   );
 };
