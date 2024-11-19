29a538ba1fa4cee22ce734fbfe915dcef39b1664
Smogg21 <spazzaglia98@gmail.com>
2024-11-18 19:17:39 -0500

    Implementación de notificaciones con react-toastify

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,7 @@ import { EditarCategoria } from "./Views/EditarCategoria";
 function App() {
   return (
     <BrowserRouter>
+      {/*Notificaciones*/}
       <NotificationListener />
       <ToastContainer />
       <Routes>
